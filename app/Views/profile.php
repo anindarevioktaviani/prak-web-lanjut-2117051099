2cba9133e02cd15cd3dba5c97228cc2d6e8a2371
aninda revi oktaviani <dwiyuniarni04@gmail.com>
2023-10-26 22:28:40 +0700

    initial Commit

---
+++ app/Views/profile.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profil Saya</title>
+</head>
+<body>
+    <h1>Profil Saya</h1>
+    <img src="gambar_profil.jpg" alt="Foto Profil Saya" width="200" height="200">
+    <h2>Nama: John Doe</h2>
+    <p>Umur: 30 tahun</p>
+    <p>Alamat: Jalan Contoh No. 123, Kota Contoh</p>
+    <p>Email: john.doe@example.com</p>
+    <p>Telepon: 123-456-7890</p>
+    <p>Deskripsi Singkat: Saya adalah seorang pengembang web yang suka belajar hal baru.</p>
+</body>
+</html>
