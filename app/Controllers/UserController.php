4b791a2acfb0f2306a732da0b26b57f8746d9bbf
aninda revi oktaviani <dwiyuniarni04@gmail.com>
2023-10-27 15:10:39 +0700

    Allahuakbar

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,9 +3,10 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-
+use App\Models\UserModel;
 class UserController extends BaseController
 {
+    protected $helpers=['Form'];
     public function index()
     {
         //
@@ -25,16 +26,68 @@ class UserController extends BaseController
     }
 
     public function create(){
-        return view('create_user');
+        $kelas = [
+            [
+                'id'=> 1,
+                'nama_kelas'=>'A'
+            ],
+            [
+                'id'=> 2,
+                'nama_kelas'=>'B'
+            ],
+            [
+                'id'=> 3,
+                'nama_kelas'=>'C'
+            ],
+            [
+                'id'=> 4,
+                'nama_kelas'=>'D'
+            ],
+        ];
+
+        $data = [
+            'kelas' => $kelas,
+        ];
+
+        return view('create_user',$data);
     }
 
     public function store(){
-        // dd($this->request->getVar());
+        $userModel = new UserModel();
+
+        if(!$this->validate([
+            'nama' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Nama harus diisi!'
+                ]
+                ],
+            'npm' => [
+                'rules' => 'required|is_unique[user.npm]',
+                'errors' => [
+                    'required' => 'NPM harus diisi!',
+                    'is_unique' => 'NPM sudah digunakan'
+                ]
+            ]
+        ])) {
+            session()->setFlashdata('error', $this->validator->listErrors());
+            return redirect()->back()->withInput();
+        }
+
+        $userModel->saveUser([
+            'nama'      => $this->request->getVar('nama'),
+            'id_kelas'  => $this->request->getVar('kelas'),
+            'npm'       => $this->request->getVar('npm'),
+        ]);
+
         $data = [
-            'nama' =>$this->request->getVar('nama'),
-            'npm' =>$this->request->getVar('npm'),
-            'kelas' =>$this->request->getVar('kelas'),
+            'nama' => $this->request->getVar('nama'),
+            'npm' => $this->request->getVar('npm'),
+            'kelas' => $this->request->getVar('kelas'),
         ];
-        return view('profile',$data);
+
+        return view('profile', $data);
     }
+
+  
 }
--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -4,37 +4,68 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #E2BCAC !important;
+        }
+        .container {
+            margin-top: 50px;
+            background-color: #f7dfd4;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        }
+        .form-control {
+            width: 60%;
+        }
+
+        .btn-custom {
+            background-color: #874e4c; /* Ganti warna sesuai preferensi Anda */
+            border-color: #E2BCAC;
+            color: #ffffff; /* Warna teks */
+        }
+        .btn-custom:hover {
+            background-color: #E2BCAC; /* Warna hover */
+            border-color: #E2BCAC;
+        }
+    </style>
     <title>Create User</title>
 </head>
 <body>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-    <title>Create User</title>
-</head>
-<body>
-    <center>
-    <div class="container mt-5">
+    <div class="container">
+        <h1 class="text-center mb-4">Form Tambah Pengguna</h1>
+        <center>
         <form action="<?= base_url('/user/store')?>" method="POST">
             <div class="mb-3">
                 <label for="nama" class="form-label">Nama</label>
-                <input type="text" class="form-control" id="nama" style="width: 20%"  name="nama">
+                <input type="text" class="form-control <?= (empty(validation_show_error('nama'))) ? '' : 'is-invalid' ?>" 
+                id="nama" name="nama" style="width: 30%" value="<?= old('nama') ?>"><?= validation_show_error('nama'); ?>
             </div>
             <div class="mb-3">
                 <label for="npm" class="form-label">NPM</label>
-                <input type="text" class="form-control" id="npm" style="width: 20%" name="npm">
+                <input type="text" class="form-control <?= (empty(validation_show_error('npm'))) ? '' : 'is-invalid' ?>" 
+                id="npm" name="npm" style="width: 30%" value="<?= old('npm') ?>"><?= validation_show_error('npm'); ?>
             </div>
             <div class="mb-3">
-                <label for="kelas" class="form-label">Kelas</label>
-                <input type="text" class="form-control" id="kelas"  style="width: 20%" name="kelas">
+                <label for="kelas" class="form-label" >Kelas</label>
+                <select class="form-select" name="kelas" id="kelas" style="width: 30%">
+                    <?php
+                    foreach ($kelas as $item){
+                        ?>
+                        <option value="<?=$item['id']?>">
+                            <?=$item['nama_kelas']?>
+                        </option>
+                        <?php
+                    }
+                    ?>
+                </select>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            </center>
+            <div class="text-center">
+                <button type="submit" class="btn btn-custom" style="width: 10%">Submit</button>
+            </div>
         </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    </center>
 </body>
 </html>
